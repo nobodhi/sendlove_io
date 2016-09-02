c661fd9613913952c6a10e5f54658f804edbb3e4
Joseph Brown <joe@sendlove.io>
2016-09-02 14:45:53 -0700

    asdf

--- app.js
+++ app.js
@@ -216,9 +216,8 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-//app.listen(app.get('port'), () => {
-//  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-//});
-app.listen(80);
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
 
 module.exports = app;
