5f2f331c2518f4b991f18ab98991f6531640029b
Joseph Brown <joe@sendlove.io>
2016-09-02 16:29:36 -0700

    changed to port 80 in app.js, line 57

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ mongoose.connection.on('error', () => {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(compression());
@@ -216,6 +216,7 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
+
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
