cb578ec9e1d469d58ea9cd0054c09c18dce3c8ff
Joseph Brown <joe@sendlove.io>
2016-10-10 19:02:41 -0700

    captured marker dragend

--- public/js/testmap.js
+++ public/js/testmap.js
@@ -9,19 +9,22 @@ function initMap() {
   deleteMarkers();
   var geocoder = new google.maps.Geocoder();
 
-  document.getElementById('submit').addEventListener('click', function() {
-   
+  
+  // press enter to set the marker 
+  document.getElementById('submit').addEventListener('click', function() { 
     deleteMarkers();
     geocodeAddress(geocoder, map);
-
   });
 
-
-  // This event listener will call addMarker() when the map is clicked.
-  map.addListener('click', function(event) {
-    addMarker(event.latLng);
-  });  
-
+  // press enter to set the marker
+  document.getElementById('address').addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      deleteMarkers();
+      geocodeAddress(geocoder, map);
+    }
+  });
+  
   // Sets the map on all markers in the array.
   function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
@@ -46,39 +49,31 @@ function geocodeAddress(geocoder, resultsMap) {
   geocoder.geocode({'address': address}, function(results, status) {
 
     if (status === 'OK') {
-      
+          
       resultsMap.setCenter(results[0].geometry.location);
-      resultsMap.setZoom(8);
+      resultsMap.setZoom(16);
       
-      markers.push (new google.maps.Marker({
+      // ok to here
+      var marker  = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location,
-        title: location.location,
+        title:  'put something here',
         icon: {
           url: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAjCAIAAABpW9/5AAAABnRSTlMAAAAAAABupgeRAAAACXBIWXMAARxbAAEcWwEUN6FaAAACMElEQVRYw8XXS0hVURTG8d89V4uiMEWwib0GlgVqYGJlgyQsKsJJkyCoYeYsgwZBRYMG2aShCRHUrBcR5UAIkuhB5ZN0koNASCsqDEsra2DJTb2Pc1PvNzrsvdb+7732Y60T8a8iVLGXMpaDt3RwjzbGJdEaaiijgIBBurjP62mWkSluZymNM2gfZ+iO01vIcarj9LZyjsGYlujkVyXNFMZfTT61vKN3WtcOmihKGIl9vIxh/wEXc4nFySIZsJ0hXsU07uE8C5L5LmInD/kwGeqF3GCVVPWDw7SDEq6QnbJvP/sZnVjxQXYLoYByrpNFM3lhfHP5QgfRKI0sEU45DFPKLqFVxFWyKiiQjg4RpOWYz2aytkpT+dLXFoL1MqANBCsyAS4kyMsEOJcgOxPgLIJvmQB/JRjIBPgNQXsmwC8IWjIBvkvwjJ75pT6mayJJ9FGb7vsXViPU83kCPMQw2+ae+pMGOmMLgW7GqZhL6ndO0Dq99HnOIFWxTbOn99TTNmPNhV4eUc6yWaU+oG5aoTl1eUPcJJuS2ThuHznNRUYSl7exWstJNqaL/MVtLvApjkEkcW1VyzFyQlIHOMWThDbRxLPu5Q4rWZ0ytYU6+pOZRVO58i0MU5ls18dppJGxFOaX6t3popPq+IX7KA3cSjkwkVCbV0wTuTPl13qehhkqEva4ruMyS2NaxjgSkpqmNtFODz10UzOfqe3AX/DRec7kEZq4NjcPe/JfoNL/cP8N53lwp5gXjtsAAAAASUVORK5CYII=',
           scaledSize: new google.maps.Size(22, 20),
         },        
         draggable: true        
-      }));
+      });
+      google.maps.event.addListener(marker, 'dragend', function(event) {
+         alert( 'Lat: ' + event.latLng.lat() + ' and Longitude is: ' + event.latLng.lng() );
+      });      
+      markers.push (marker);
     } else {
-      alert('Locating was not successful for the following reason: ' + status);
+      alert('Location was not successful for the following reason: ' + status);
     }
   });
+  
 
+  
 }
 
-
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map
-  });
-  markers.push(marker);
-  //get marker position and store in hidden input
-  google.maps.event.addListener(marker, 'dragend', function (evt) {
-      document.getElementById("latitude").value = evt.latLng.lat().toFixed(3);
-      document.getElementById("longitude").value = evt.latLng.lng().toFixed(3);
-  });
-    
-}  
